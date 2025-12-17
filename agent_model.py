5871d28548b7a659bf0e58d845c766d4a8b7f3a0
Mark kibocha <regexmark1@gmail.com>
2025-12-17 13:07:25 +0300

    Change model initialization to use gpt-5-nano

--- agent_model.py
+++ agent_model.py
@@ -2,5 +2,5 @@ from langchain.chat_models import init_chat_model
 import os
 
 
-model = init_chat_model("gemini-2.0-flash", model_provider="google_genai",
+model = init_chat_model("gpt-5-nano", model_provider="openai",
  api_key=os.getenv("GOOGLE_API_KEY"))
